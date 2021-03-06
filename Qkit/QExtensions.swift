93fdb67a11dd07bd1769be918088782a3d74e81f
Raul Sulaimanov <raul.sulaimanov@gmail.com>
2021-03-06 12:16:40 +0100

    added extensions class

---
+++ Qkit/QExtensions.swift
@@ -0,0 +1,43 @@
+//
+//  QExtensions.swift
+//  Qkit
+//
+//  Created by Raul Sulaimanov on 06.03.21.
+//
+
+import Foundation
+import UIKit
+
+extension CGFloat {
+    static func random() -> CGFloat {
+        return CGFloat(arc4random()) / CGFloat(UInt32.max)
+    }
+}
+
+extension UIColor {
+    public convenience init?(hex: String) {
+        let r, g, b, a: CGFloat
+
+        if hex.hasPrefix("#") {
+            let start = hex.index(hex.startIndex, offsetBy: 1)
+            let hexColor = String(hex[start...])
+
+            if hexColor.count == 8 {
+                let scanner = Scanner(string: hexColor)
+                var hexNumber: UInt64 = 0
+
+                if scanner.scanHexInt64(&hexNumber) {
+                    r = CGFloat((hexNumber & 0xFF00_0000) >> 24) / 255
+                    g = CGFloat((hexNumber & 0x00FF_0000) >> 16) / 255
+                    b = CGFloat((hexNumber & 0x0000_FF00) >> 8) / 255
+                    a = CGFloat(hexNumber & 0x0000_00FF) / 255
+
+                    self.init(red: r, green: g, blue: b, alpha: a)
+                    return
+                }
+            }
+        }
+
+        return nil
+    }
+}
